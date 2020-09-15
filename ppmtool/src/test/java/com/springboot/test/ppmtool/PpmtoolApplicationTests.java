9a5580cd7a661ed98fc9dbf1b855426361e6b9b9
PRIYA09910714 <pri1490@gmail.com>
2020-09-15 19:13:55 +0530

    first commit

---
+++ ppmtool/src/test/java/com/springboot/test/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.test.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
